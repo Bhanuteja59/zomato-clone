ab0cb9d9a19ff5b8e7586d3cc513766810fe5ed8
Bhanu teja <144136452+Bhanuteja59@users.noreply.github.com>
2024-12-05 19:09:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,18 @@ var login_clicked = document.querySelector(".login_clicked");
 var signup_clicked = document.querySelector(".signup_clicked");
 var login_submit = document.querySelector(".login-submit");
 var signUp_submit = document.querySelector(".signUp-submit");
+var delivary = document.querySelector(".delivary");
+var diningOut = document.querySelector(".dining_out");
+var nightLife = document.querySelector(".night_life");
+var main = document.querySelector(".main");
+var pages = document.querySelector(".pages");
+var delivaryPage = document.querySelector(".delivary_page");
+var diningOutPage = document.querySelector(".dining_out_page");
+var nightOutPage = document.querySelector(".night_life_page");
+var pageOut = document.querySelector(".pageOut");
+var pageOut2 = document.querySelector(".pageOut2");
+var pageOut3 = document.querySelector(".pageOut3");
+
 
 login.addEventListener("click", () => {
     console.log("Login clicked");
@@ -36,6 +48,7 @@ login_submit.addEventListener("click", (e) => {
         alert("Login Successful!");
         console.log(username, " , " , password);
     }
+  login_clicked.style.display = "none";
 });
 
 signUp_submit.addEventListener("click", (e) => {
@@ -45,7 +58,7 @@ signUp_submit.addEventListener("click", (e) => {
     var password = document.querySelector("#signup-password").value.trim();
     var confirmPassword = document.querySelector("#confirm-password").value.trim();
 
-    if (!username || !email || !password || !confirmPassword) {
+    if (!username && !email && !password && !confirmPassword) {
         alert("Please fill in all the fields.");
     } else if (password !== confirmPassword) {
         alert("Passwords do not match.");
@@ -53,4 +66,47 @@ signUp_submit.addEventListener("click", (e) => {
         alert("Signup Successful!");
         console.log(username , " , ", email , " , ", password);
     }
+  signup_clicked.style.display = "none";
 });
+
+delivary.addEventListener("click", () => {
+    console.log("Deliveries selected");
+    main.style.display = "none";
+    pages.style.display = "block";
+    delivaryPage.style.display = "block";
+    diningOutPage.style.display = "none";
+    nightOutPage.style.display = "none";
+});
+
+diningOut.addEventListener("click", () => {
+    console.log("dining out is selected");
+    main.style.display = "none";
+    pages.style.display = "block";
+    delivaryPage.style.display = "none";
+    diningOutPage.style.display = "block";
+    nightOutPage.style.display = "none";
+});
+
+nightLife.addEventListener("click", () => {
+    console.log("night out selected");
+    main.style.display = "none";
+    pages.style.display = "block";
+    delivaryPage.style.display = "none";
+    diningOutPage.style.display = "none";
+    nightOutPage.style.display = "block";
+});
+
+pageOut.addEventListener("click",()=>{
+    pages.style.display = "none";
+    main.style.display = "block";
+})
+
+pageOut2.addEventListener("click",()=>{
+    pages.style.display = "none";
+    main.style.display = "block";
+})
+
+pageOut3.addEventListener("click",()=>{
+    pages.style.display = "none";
+    main.style.display = "block";
+})
